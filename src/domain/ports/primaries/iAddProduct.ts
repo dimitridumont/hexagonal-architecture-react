f9cfedd602a09075d3d8414c02f406c94fa38259
Dimitri Dumont <contact@srdigitale.fr>
2020-09-16 16:06:12 +0200

    Remove unused interface right now

--- src/domain/ports/primaries/iAddProduct.ts
+++
@@ -1,5 +0,0 @@
-import {Product} from "../../entities"
-
-export interface IAddProduct {
-    addProduct(product: Product): Product
-}
--- src/domain/ports/primaries/index.ts
+++ src/domain/ports/primaries/index.ts
@@ -1,2 +1,1 @@
 export * from "./iGetProducts"
-export * from "./iAddProduct"
